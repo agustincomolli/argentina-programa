fef5189248a86858214ed092032a258d1cb83a7e
Agustín Comolli <agustincomolli@yahoo.com.ar>
2023-09-12 16:06:36 -0300

    Agregado ejercicio 1 de js

---
+++ JavaScript/01-sintaxis-basica.js
@@ -0,0 +1,21 @@
+// Muestra un mensaje en la consola.
+console.log("¡Hola mundo!\n")
+
+// Crear una constante con el valor 3.
+const number = 3;  // los punto y coma ";" son una buena práctica de programación.
+
+// Imprimir el número en la consola.
+console.log(number, "\n");
+
+
+// Operaciones artiméticas.
+
+const addition = 2 + 2;
+const substraction = 5 - 2;
+const multiplication = 2 * 3;
+const division = 10 / 4;
+
+console.log("2 + 2  = ", addition);
+console.log("5 - 2  = ", substraction);
+console.log("2 * 3  = ", multiplication);
+console.log("10 / 4 = ", division);
