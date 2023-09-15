e9a1d2f4e3453a8946fe820241e5c3a517daa76f
Agustín Comolli <agustincomolli@gmail.com>
2023-09-15 11:26:11 -0300

    Agregado variables en JS

---
+++ JavaScript/02-variables.js
@@ -0,0 +1,27 @@
+console.log("Variables y tipos de datos primitivos:\n")
+// Variables indefinidas o undefined
+let my_var;
+console.log("my_var es un tipo de variable: ", typeof my_var);
+
+// Variables booleanas (true o false)
+let is_valid = true;
+console.log("is_valid es un tipo de variable: ", typeof is_valid);
+
+// Variables numéricas (integer y float)
+let students = 10;
+console.log("students es un tipo de variable: ", typeof students);
+let income = 4.5;
+console.log("income es un tipo de variable: ", typeof income);
+
+// Variables de cadena o strings
+let username = "agustincomolli";
+console.log("username es un tipo de variable: ", typeof username);
+
+// Variable de valor nulo o null (no existe)
+let eight_day_of_week = null;
+console.log("eight_day_of_week es un tipo de variable: ", typeof eight_day_of_week);
+
+// Las constantes son variables que no cambian.
+// Deben declararse y asignarse a la vez.
+const pi = 3.1416;
+console.log("pi = ", pi)
