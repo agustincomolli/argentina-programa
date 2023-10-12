82f40b6f78c9ce14aba9676fa1308abf853337a8
Agustín Comolli <agustincomolli@gmail.com>
2023-10-12 10:52:35 -0300

    feat(web): add language selector and manipulate the dom

--- jquery/01-introduccion/index.js
+++ jquery/01-introduccion/index.js
@@ -1,3 +1,31 @@
-$("#btn-clickme").on("click", function() {
-    document.writeln("<h2>¡Bienvenido a JQuery!</h2>");
+function showGreetings(language){
+    let greet = "";
+    if (language === "es") {
+        greet = "¡Bienvenido a JQuery!";
+    } else {
+        greet = "Welcome to JQuery";
+    };
+
+    return greet;
+};
+
+$(document).ready(() => { 
+    const selectLanguage = $("#language");
+    const btnClickMe = $("#btn-clickme");
+    
+    btnClickMe.on("click", ()=>{
+        let message = showGreetings(selectLanguage.val());
+        $("h1").text(message);
+        $("#hided-paragraph").toggle();
+    });
+
+    const comments = $(".comments"); // Array de elementos.
+    comments.each((index) => {
+        const btnDelete = $(comments[index]).find("button");
+        btnDelete.on("click", function() {
+            const parent = $(this).closest(".comment");
+            parent.remove();
+        });
+    });
 });
+
